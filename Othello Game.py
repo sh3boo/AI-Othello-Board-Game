61d20b0c9c4d4296166bb7d0425260efef118e60
adel-3 <aboadel1512003@gmail.com>
2024-05-14 15:01:26 +0300

    change the background and other changes

--- Othello Game.py
+++ Othello Game.py
@@ -1,6 +1,227 @@
 import pygame
-import random
+
 import copy
+
+
+# Knowledge Representation of Game State
+class Grid:
+    def __init__(self, rows, cols, size, main):
+        self.GAME = main
+        self.y = rows
+        self.x = cols
+        self.size = size
+
+        self.white = loadScaledImage('photos/white.png', size)
+        self.black = loadScaledImage('photos/black.png', size)
+        self.whiteToBlack = [loadScaledImage(f'photos/BlackToWhite{i}.png', self.size) for i in range(1, 4)]
+        self.blackToWhite = [loadScaledImage(f'photos/WhiteToBlack{i}.png', self.size) for i in range(1, 4)]
+        self.bg = self.loadBackgroundImage()
+
+        self.tokens = {}
+        self.gridBackground = self.createBackgroundImage()
+        self.gridLogic = self.regenGrid(self.y, self.x)
+
+        self.firstPlayerScore = 0
+        self.secondPlayerScore = 0
+
+        self.font = pygame.font.SysFont('Arial', 20, True, False)
+
+    def startNewGame(self):
+        self.tokens.clear()
+        self.gridLogic = self.regenGrid(self.y, self.x)
+
+    def loadBackgroundImage(self):
+        alpha = 'ABCDEFGHI'
+        spriteSheet = pygame.image.load('photos/backGround.png').convert_alpha()
+        imageDict = {}
+        for i in range(3):
+            for j in range(7):
+                imageDict[alpha[j]+str(i)] = loadSpriteSheet(spriteSheet, j, i, (self.size), (32, 32))
+        return imageDict
+
+    def createBackgroundImage(self):
+        gridBackground = [
+            ['C0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'E0'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
+            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
+            ['C2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'E2'],
+        ]
+        image = pygame.Surface((960, 960))
+        for j, row in enumerate(gridBackground):
+            for i, img in enumerate(row):
+                image.blit(self.bg[img], (i * self.size[0], j * self.size[1]))
+        return image
+
+    def regenGrid(self, rows, cols):
+        grid = []
+        for y in range(rows):
+            line = []
+            for x in range(cols):
+                line.append(0)
+            grid.append(line)
+        self.addCurPlayerToken(grid, 1, 3, 3)
+        self.addCurPlayerToken(grid, -1, 3, 4)
+        self.addCurPlayerToken(grid, 1, 4, 4)
+        self.addCurPlayerToken(grid, -1, 4, 3)
+
+        return grid
+
+    def calcPlyerScore(self, player):
+        score = 0
+        for row in self.gridLogic:
+            for col in row:
+                if col == player:
+                    score += 1
+        return score
+
+    def drawScore(self, curPlayer, playerScore):
+        textImage = self.font.render(f'{curPlayer} : {playerScore}', 1, 'White')
+        return textImage
+
+    def endGameScreen(self):
+        if self.GAME.endGame:
+            finishGameScreen = pygame.Surface((320, 320))
+            finishMessage = self.font.render(f'{"yabn ella3eba,ent elle ksebt" if self.firstPlayerScore > self.secondPlayerScore else "ent elle 5esert ya zbooon"}', 1, 'White')
+            finishGameScreen.blit(finishMessage, (0, 0))
+            startNewGame = pygame.draw.rect(finishGameScreen, 'White', (80, 160, 160, 80))
+            newRoundMessage = self.font.render('ell3ab game kaman', 1, 'Black')
+            finishGameScreen.blit(newRoundMessage, (85, 190))
+        return finishGameScreen
+
+
+    def drawGrid(self, window):
+        window.blit(self.gridBackground, (0, 0))
+
+        window.blit(self.drawScore('Computer Player', self.firstPlayerScore), (900, 300))
+        window.blit(self.drawScore('You', self.secondPlayerScore), (900, 400))
+
+        for token in self.tokens.values():
+            token.renderGameOnScreen(window)
+
+        availableMoves = self.availableMoves(self.gridLogic, self.GAME.currPlayer)
+        if self.GAME.currPlayer == 1:
+            for move in availableMoves:
+                pygame.draw.rect(window, 'White', (80 + (move[1] * 80) + 30, 80 + (move[0] * 80) + 30, 20, 20))
+
+        if self.GAME.endGame:
+            window.blit(self.endGameScreen(), (240, 240))
+
+    """def printGameLogicBoard(self):
+        print('  | A | B | C | D | E | F | G | H |')
+        for i, row in enumerate(self.gridLogic):
+            line = f'{i} |'.ljust(3, " ")
+            for item in row:
+                line += f"{item}".center(3, " ") + '|'
+            print(line)
+        print()"""
+
+    def determineAvailCells(self, grid, curPlayer):
+        availCellsToMove = []
+        for gridX, row in enumerate(grid):
+            for gridY, col in enumerate(row):
+                if grid[gridX][gridY] != 0:
+                    continue
+                validDirections = directions(gridX, gridY)
+
+                for direction in validDirections:
+                    dirX, dirY = direction
+                    checkedCell = grid[dirX][dirY]
+
+                    if checkedCell == 0 or checkedCell == curPlayer:
+                        continue
+
+                    if (gridX, gridY) in availCellsToMove:
+                        continue
+
+                    availCellsToMove.append((gridX, gridY))
+        return availCellsToMove
+
+    def swappableTiles(self, x, y, grid, player):
+        availableCells = directions(x, y)
+        if len(availableCells) == 0:
+            return []
+
+        swappableTiles = []
+        for availablecell in availableCells:
+            check_x, check_y = availablecell
+            dif_x, dif_y = check_x - x, check_y - y
+            current_line = []
+
+            RUN = True
+            while RUN:
+                if grid[check_x][check_y] == player * -1:
+                    current_line.append((check_x, check_y))
+                elif grid[check_x][check_y] == player:
+                    RUN = False
+                    break
+                elif grid[check_x][check_y] == 0:
+                    current_line.clear()
+                    RUN = False
+                check_x += dif_x
+                check_y += dif_y
+
+                if check_x < 0 or check_x > 7 or check_y < 0 or check_y > 7:
+                    current_line.clear()
+                    RUN = False
+
+            if len(current_line) > 0:
+                swappableTiles.extend(current_line)
+
+        return swappableTiles
+
+    def availableMoves(self, grid, currPlayer):
+
+        availableMOvesCells = self.determineAvailCells(grid, currPlayer)
+        availableToChooseCells = []
+
+        for cell in availableMOvesCells:
+            x, y = cell
+            if cell in availableToChooseCells:
+                continue
+            changeTiles = self.swappableTiles(x, y, grid, currPlayer)
+
+            if len(changeTiles) > 0:
+                availableToChooseCells.append(cell)
+
+        return availableToChooseCells
+
+    def addCurPlayerToken(self, grid, curPlayer, y, x):
+        curPlayerTokenImage = self.white if curPlayer == 1 else self.black
+        self.tokens[(y, x)] = Token(curPlayer, y, x, curPlayerTokenImage, self.GAME)
+        grid[y][x] = self.tokens[(y, x)].player
+
+    def transition(self, cell, player):
+        if player == 1:
+            self.tokens[(cell[0], cell[1])].transition(self.whiteToBlack, self.white)
+        else:
+            self.tokens[(cell[0], cell[1])].transition(self.blackToWhite, self.black)
+
+
+class Token:
+    def __init__(self, player, gridX, gridY, image, main):
+        self.player = player
+        self.gridX = gridX
+        self.gridY = gridY
+        self.pos_x = 80 + (gridY * 80)
+        self.pos_y = 80 + (gridX * 80)
+        self.GAME = main
+
+        self.image = image
+
+    def transition(self, transitionImages, curPlayerTokenImage):
+        for i in range(30):
+            self.image =  transitionImages[i // 10]
+            self.GAME.renderGameOnScreen()
+        self.image = curPlayerTokenImage
+
+    def renderGameOnScreen(self, window):
+        window.blit(self.image, (self.pos_x, self.pos_y))
 
 #  utility functions
 def directions(x, y, minX=0, minY=0, maxX=7, maxY=7):
@@ -33,7 +254,7 @@ def loadScaledImage(path, size):
     return img
 
 def loadSpriteSheet(sheet, row, col, new_size, size):
-    image = pygame.Surface((35, 35)).convert_alpha()
+    image = pygame.Surface((33, 33)).convert_alpha()
     image.blit(sheet, (0, 0), (row * size[0], col * size[1], size[0], size[1]))
     image = pygame.transform.scale(image, new_size)
     image.set_colorkey('Black')
@@ -148,224 +369,7 @@ class mainGameClass:
         pygame.display.update()
 
 
-# Knowledge Representation of Game State
-class Grid:
-    def __init__(self, rows, cols, size, main):
-        self.GAME = main
-        self.y = rows
-        self.x = cols
-        self.size = size
 
-        self.white = loadScaledImage('photos/white.png', size)
-        self.black = loadScaledImage('photos/black.png', size)
-        self.whiteToBlack = [loadScaledImage(f'photos/BlackToWhite{i}.png', self.size) for i in range(1, 4)]
-        self.blackToWhite = [loadScaledImage(f'photos/WhiteToBlack{i}.png', self.size) for i in range(1, 4)]
-        self.bg = self.loadBackgroundImage()
-
-        self.tokens = {}
-        self.gridBackground = self.createBackgroundImage()
-        self.gridLogic = self.regenGrid(self.y, self.x)
-
-        self.firstPlayerScore = 0
-        self.secondPlayerScore = 0
-
-        self.font = pygame.font.SysFont('Arial', 20, True, False)
-
-    def startNewGame(self):
-        self.tokens.clear()
-        self.gridLogic = self.regenGrid(self.y, self.x)
-
-    def loadBackgroundImage(self):
-        alpha = 'ABCDEFGHI'
-        spriteSheet = pygame.image.load('photos/backGround.png').convert_alpha()
-        imageDict = {}
-        for i in range(3):
-            for j in range(7):
-                imageDict[alpha[j]+str(i)] = loadSpriteSheet(spriteSheet, j, i, (self.size), (32, 32))
-        return imageDict
-
-    def createBackgroundImage(self):
-        gridBackground = [
-            ['C0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'D0', 'E0'],
-            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
-            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
-            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
-            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
-            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
-            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
-            ['C1', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'E1'],
-            ['C1', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'B0', 'A0', 'E1'],
-            ['C2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'D2', 'E2'],
-        ]
-        image = pygame.Surface((960, 960))
-        for j, row in enumerate(gridBackground):
-            for i, img in enumerate(row):
-                image.blit(self.bg[img], (i * self.size[0], j * self.size[1]))
-        return image
-
-    def regenGrid(self, rows, cols):
-        grid = []
-        for y in range(rows):
-            line = []
-            for x in range(cols):
-                line.append(0)
-            grid.append(line)
-        self.addCurPlayerToken(grid, 1, 3, 3)
-        self.addCurPlayerToken(grid, -1, 3, 4)
-        self.addCurPlayerToken(grid, 1, 4, 4)
-        self.addCurPlayerToken(grid, -1, 4, 3)
-
-        return grid
-
-    def calcPlyerScore(self, player):
-        score = 0
-        for row in self.gridLogic:
-            for col in row:
-                if col == player:
-                    score += 1
-        return score
-
-    def drawScore(self, curPlayer, playerScore):
-        textImage = self.font.render(f'{curPlayer} : {playerScore}', 1, 'White')
-        return textImage
-
-    def endGameScreen(self):
-        if self.GAME.endGame:
-            finishGameScreen = pygame.Surface((320, 320))
-            finishMessage = self.font.render(f'{"yabn ella3eba,ent elle ksebt" if self.firstPlayerScore > self.secondPlayerScore else "ent elle 5esert ya zbooon"}', 1, 'White')
-            finishGameScreen.blit(finishMessage, (0, 0))
-            startNewGame = pygame.draw.rect(finishGameScreen, 'White', (80, 160, 160, 80))
-            newRoundMessage = self.font.render('ell3ab game kaman', 1, 'Black')
-            finishGameScreen.blit(newRoundMessage, (85, 190))
-        return finishGameScreen
-
-
-    def drawGrid(self, window):
-        window.blit(self.gridBackground, (0, 0))
-
-        window.blit(self.drawScore('White', self.firstPlayerScore), (900, 100))
-        window.blit(self.drawScore('Black', self.secondPlayerScore), (900, 200))
-
-        for token in self.tokens.values():
-            token.renderGameOnScreen(window)
-
-        availableMoves = self.availableMoves(self.gridLogic, self.GAME.currPlayer)
-        if self.GAME.currPlayer == 1:
-            for move in availableMoves:
-                pygame.draw.rect(window, 'White', (80 + (move[1] * 80) + 30, 80 + (move[0] * 80) + 30, 20, 20))
-
-        if self.GAME.endGame:
-            window.blit(self.endGameScreen(), (240, 240))
-
-    """def printGameLogicBoard(self):
-        print('  | A | B | C | D | E | F | G | H |')
-        for i, row in enumerate(self.gridLogic):
-            line = f'{i} |'.ljust(3, " ")
-            for item in row:
-                line += f"{item}".center(3, " ") + '|'
-            print(line)
-        print()"""
-
-    def determineAvailCells(self, grid, curPlayer):
-        availCellsToMove = []
-        for gridX, row in enumerate(grid):
-            for gridY, col in enumerate(row):
-                if grid[gridX][gridY] != 0:
-                    continue
-                validDirections = directions(gridX, gridY)
-
-                for direction in validDirections:
-                    dirX, dirY = direction
-                    checkedCell = grid[dirX][dirY]
-
-                    if checkedCell == 0 or checkedCell == curPlayer:
-                        continue
-
-                    if (gridX, gridY) in availCellsToMove:
-                        continue
-
-                    availCellsToMove.append((gridX, gridY))
-        return availCellsToMove
-
-    def swappableTiles(self, x, y, grid, player):
-        availableCells = directions(x, y)
-        if len(availableCells) == 0:
-            return []
-
-        swappableTiles = []
-        for availablecell in availableCells:
-            check_x, check_y = availablecell
-            dif_x, dif_y = check_x - x, check_y - y
-            current_line = []
-
-            RUN = True
-            while RUN:
-                if grid[check_x][check_y] == player * -1:
-                    current_line.append((check_x, check_y))
-                elif grid[check_x][check_y] == player:
-                    RUN = False
-                    break
-                elif grid[check_x][check_y] == 0:
-                    current_line.clear()
-                    RUN = False
-                check_x += dif_x
-                check_y += dif_y
-
-                if check_x < 0 or check_x > 7 or check_y < 0 or check_y > 7:
-                    current_line.clear()
-                    RUN = False
-
-            if len(current_line) > 0:
-                swappableTiles.extend(current_line)
-
-        return swappableTiles
-
-    def availableMoves(self, grid, currPlayer):
-
-        availableMOvesCells = self.determineAvailCells(grid, currPlayer)
-        availableToChooseCells = []
-
-        for cell in availableMOvesCells:
-            x, y = cell
-            if cell in availableToChooseCells:
-                continue
-            changeTiles = self.swappableTiles(x, y, grid, currPlayer)
-
-            if len(changeTiles) > 0:
-                availableToChooseCells.append(cell)
-
-        return availableToChooseCells
-
-    def addCurPlayerToken(self, grid, curPlayer, y, x):
-        curPlayerTokenImage = self.white if curPlayer == 1 else self.black
-        self.tokens[(y, x)] = Token(curPlayer, y, x, curPlayerTokenImage, self.GAME)
-        grid[y][x] = self.tokens[(y, x)].player
-
-    def transition(self, cell, player):
-        if player == 1:
-            self.tokens[(cell[0], cell[1])].transition(self.whiteToBlack, self.white)
-        else:
-            self.tokens[(cell[0], cell[1])].transition(self.blackToWhite, self.black)
-
-class Token:
-    def __init__(self, player, gridX, gridY, image, main):
-        self.player = player
-        self.gridX = gridX
-        self.gridY = gridY
-        self.pos_x = 80 + (gridY * 80)
-        self.pos_y = 80 + (gridX * 80)
-        self.GAME = main
-
-        self.image = image
-
-    def transition(self, transitionImages, curPlayerTokenImage):
-        for i in range(30):
-            self.image =  transitionImages[i // 10]
-            self.GAME.renderGameOnScreen()
-        self.image = curPlayerTokenImage
-
-    def renderGameOnScreen(self, window):
-        window.blit(self.image, (self.pos_x, self.pos_y))
 
 class ComputerPlayer:
     def __init__(self, grid_object):
